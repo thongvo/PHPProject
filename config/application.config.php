572b338a757bda312bcb246690af84244b7b0f5c
thongvo <hoangthongvo_1993@yahoo.com.vn>
2015-10-26 16:24:03 +0700

    Add new module for manage Subject.

--- config/application.config.php
+++ config/application.config.php
@@ -8,6 +8,7 @@ return array(
         'Department',
         'Student',
         'Instructor',
+        'Subject',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
